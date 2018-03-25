49f09e9cff7e013b5a7c5801ef89e82006eae7a1
lookskystar <lookskystar@163.com>
2018-03-25 11:37:04 +0800

    第二次提交测试

--- src/com/andy/demo/HelloWorld.java
+++ src/com/andy/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.andy.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("版本控制器来了");
     }
 }
